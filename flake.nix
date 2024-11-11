467fa432c92a972345e73891f47bb74a57197564
darbster145 <swoopmaster22@proton.me>
2024-11-10 19:41:16 -0700

    added x64 dmg

--- flake.nix
+++ flake.nix
@@ -7,20 +7,31 @@
 
   outputs = { self, nixpkgs }: {
     packages = {
-      darwin.aarch64 = let
-        pkgs = import nixpkgs { system = "aarch64-darwin"; };
+      darwin = forEachSystem (system: let
+        pkgs = import nixpkgs { inherit system; };
 
         pname = "zen";
         version = "1.0.1-a.19";
         appName = "Zen Browser.app";
+
+        # Define architecture-specific information
+        platformData = if system == "aarch64-darwin" then {
+          url = "https://github.com/zen-browser/desktop/releases/download/${version}/zen.macos-aarch64.dmg";
+          hash = "sha256-Io95pXzIB3r0bOVAcAizIZPhsBgj6vf6J6IcOfNAtM8=";
+        } else if system == "x86_64-darwin" then {
+          url = "https://github.com/zen-browser/desktop/releases/download/${version}/zen.macos-x64.dmg";
+          hash = "sha256-0f3973a0454f65212bf37ebcf0e9eaf56f5b129cf5c2884beabdbe32dd82c5d9";
+        } else {
+          throw "Unsupported system: ${system}";
+        };
       in
       pkgs.stdenv.mkDerivation {
         pname = pname;
         version = version;
 
         src = pkgs.fetchurl {
-          url = "https://github.com/zen-browser/desktop/releases/download/${version}/zen.macos-aarch64.dmg";
-          hash = "sha256-Io95pXzIB3r0bOVAcAizIZPhsBgj6vf6J6IcOfNAtM8=";
+          url = platformData.url;
+          hash = platformData.hash;
         };
 
         nativeBuildInputs = [ pkgs.undmg pkgs.makeWrapper ];
@@ -51,10 +62,11 @@
           description = "Zen Browser";
           homepage = "https://zen-browser.app/";
           license = licenses.free;
-          platforms = [ "aarch64-darwin" ];
+          platforms = [ "aarch64-darwin" "x86_64-darwin" ];
         };
-      };
+      });
     };
   };
 }
+
 
